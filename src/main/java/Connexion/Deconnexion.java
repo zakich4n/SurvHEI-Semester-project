fd282162d4fd5534e39fc950628a130fff3b3d87
Hicham EL KADAOUI <hicham.el-kadaoui@student.junia.com>
2021-11-02 17:08:11 +0100

    add Deconnexion / NewUtil

--- src/main/java/Connexion/Deconnexion.java
+++ src/main/java/Connexion/Deconnexion.java
@@ -1,2 +1,14 @@
-package Connexion;public class Deconnexion {
+package Connexion;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet("/servlet/Deconnect")
+public class Deconnexion extends HttpServlet {
+    public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        HttpSession session = req.getSession(true);
+        session.invalidate();
+        res.sendRedirect("../login.html");
+    }
 }
