904a78cfb733cf6e1325f2a64238d0f8dd20d01d
Hicham EL KADAOUI <hicham.el-kadaoui@student.junia.com>
2021-11-02 16:35:03 +0100

    login

---
+++ src/main/java/Connexion/Utilisateur.java
@@ -0,0 +1,29 @@
+public class Utilisateur {
+    public String login;
+    public String mdp;
+    public String nom;
+    public String prenom;
+    public String role;
+
+    public Utilisateur(String login, String mdp,String nom, String prenom, String role){
+        maj(login,mdp,nom,prenom,role);
+    }
+
+    public void maj(String login, String mdp,String nom, String prenom, String role){
+        this.login = login;
+        this.mdp = mdp;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.role = role;
+    }
+
+    public void maj(String login, String mdp,String nom, String prenom){
+        this.login = login;
+        this.mdp = mdp;
+        this.nom = nom;
+        this.prenom = prenom;
+        // On n'autorise pas a changer le role
+    }
+
+
+}
