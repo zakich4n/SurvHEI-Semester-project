0fc34ba11dad9aa011aca9c1ee5258f5b734d605
Zaki Gouizi <zaki.gouizi@student.junia.com>
2021-11-03 15:53:37 +0100

    Edit du Readme, ajout class Page et le premier type de reponse du formulaire Oui ou Non

---
+++ src/main/java/Formulaire/Page.java
@@ -0,0 +1,11 @@
+package Formulaire;
+
+public class Page {
+    public int Numero;
+    public String Question;
+
+    public Page(int num, String Q) {
+        this.Numero=num;
+        this.Question=Q;
+    }
+}
---
+++ src/main/java/Formulaire/YesOrNO.java
@@ -0,0 +1,8 @@
+package Formulaire;
+
+public class YesOrNO extends Page{
+
+    public YesOrNO(int num, String Question, boolean YorN) {
+        super(num,Question);
+    }
+}
