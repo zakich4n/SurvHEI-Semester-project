416ad289cc23b0aea04e710b2c1bb36c0ec32c70
JTBlanqui <julien-tien.blanqui@student.junia.com>
2021-11-09 17:18:03 +0100

    Add html inscription, css inscription

--- src/main/java/Servlets/SignUpServlet.java
+++ src/main/java/Servlets/SignUpServlet.java
@@ -1,15 +1,29 @@
 package Servlets;
 
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "SignUpServlet", value = "/SignUpServlet")
+@WebServlet("/SignUp")
 public class SignUpServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContextTemplateResolver resolver = new ServletContextTemplateResolver(req.getServletContext());
+        resolver.setPrefix("/WEB-INF/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
 
+        TemplateEngine engine = new TemplateEngine();
+        engine.setTemplateResolver(resolver);
+
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        engine.process("SignUp", context, resp.getWriter());
     }
 
     @Override
