9d9c68e6b47ef2bfc819c4159dedbf481cea2cff
JTBlanqui <julien-tien.blanqui@student.junia.com>
2021-11-16 11:47:43 +0100

    Le service

---
+++ src/main/java/service/LoginService.java
@@ -0,0 +1,24 @@
+package service;
+
+import DAO.LoginDao;
+import entity.Utilisateur;
+
+public class LoginService {
+
+    private LoginDao logindao = new LoginDao();
+
+    private static class LoginServiceHolder{
+        private static LoginService instance = new LoginService();
+    }
+    public static LoginService getInstance(){return LoginServiceHolder.instance;}
+
+    private LoginService(){}
+
+    public boolean valider(Utilisateur utilisateur){return logindao.valider(utilisateur);}
+
+
+    public void addUser(Utilisateur utilisateur) {
+        LoginDao.addUser(utilisateur);
+    }
+
+}
