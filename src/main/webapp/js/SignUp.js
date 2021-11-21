6779d5aa91cd9e21a8045f211b0c5b1f0a8dda9a
JTBlanqui <julien-tien.blanqui@student.junia.com>
2021-11-21 01:39:38 +0100

    correction

---
+++ src/main/webapp/js/SignUp.js
@@ -0,0 +1,47 @@
+
+
+document.getElementById("SignupForm").addEventListener("submit", function(e) {
+
+    e.preventDefault();
+    var user = document.getElementById("user");
+    var email = document.getElementById("email");
+    var nom = document.getElementById("nom");
+    var prenom = document.getElementById("prenom");
+    var datenaissance = document.getElementById("datenaissance");
+    var mdp = document.getElementById("mdp");
+    var mdpc = document.getElementById("mdpc");
+
+    if(mdp.value != mdpc.value){
+        erreur="Veuillez entrer deux mots de passe identiques";
+    }
+    if(!mdpc.value){
+        erreur="Veuillez confirmer votre mot de passe";
+    }
+    if(!mdp.value){
+        erreur="Veuillez entrer votre mot de passe";
+    }
+    if(!datenaissance.value){
+        erreur = "Veuillez entrer votre date de naissance";
+    }
+    if(!prenom.value){
+        erreur = "Veuillez entrer votre prénom";
+    }
+    if(!nom.value){
+        erreur = "Veuillez entrer votre nom";
+    }
+    if(!email.value){
+        erreur = "Veuillez entrer votre adresse email";
+    }
+    if(!user.value){
+        erreur = "Veuillez entrer votre user";
+    }
+    if(erreur){
+        e.preventDefault();
+        document.getElementById("erreur").innerHTML=erreur ;
+        erreur=null;
+        return false;
+    }else{
+        alert('Successful Registration !');
+    }
+})
+
