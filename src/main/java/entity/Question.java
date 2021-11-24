0172a5cb758983241d3bbde30f3b5df5ede6f101
JTBlanqui <julien-tien.blanqui@student.junia.com>
2021-11-24 15:27:28 +0100

    ajout des questions du formualire vers base de données, avec nombre de questions fixes 3, et il faut ajouter l'id formulaire automatiquement

---
+++ src/main/java/entity/Question.java
@@ -0,0 +1,20 @@
+package entity;
+
+public class Question {
+    private String question;
+    private int ordre_question;
+    private int id_formulaire_correspondant;
+    private boolean obligatoire;
+
+    public Question (String question, int ordre_question, int id_formulaire_correspondant, boolean obligatoire){
+        this.question = question;
+        this.ordre_question = ordre_question;
+        this.id_formulaire_correspondant = id_formulaire_correspondant;
+        this.obligatoire = obligatoire;
+    }
+
+    public String getQuestion(){ return this.question;}
+    public int getOrdre_question(){return this.ordre_question;}
+    public int getId_formulaire_correspondant(){return this.id_formulaire_correspondant;}
+    public boolean getObligatoire(){return this.obligatoire;}
+}
--- src/test/java/ConnexionTest/AddUserTest.java
+++ src/test/java/ConnexionTest/AddUserTest.java
@@ -1,7 +1,41 @@
 package ConnexionTest;
 
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.time.Month;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AddUserTest {
 
+/*
+    @Test
+    public void shouldAddFilm() throws Exception {
+        // GIVEN
+        Film filmToCreate = new Film(null, "My new film", LocalDate.of(2019, Month.OCTOBER, 20),
+                new Genre(1, "Drama"), 123, "my new director", "my new summary");
+        // WHEN
+        Film filmCreated = filmDao.addFilm(filmToCreate);
+        // THEN
+        try (Connection connection = DataSourceProvider.getDataSource().getConnection();
+             PreparedStatement stmt = connection.prepareStatement("SELECT * FROM film WHERE film_id = ?")) {
+            stmt.setInt(1, filmCreated.getId());
+            try (ResultSet rs = stmt.executeQuery()) {
+                assertThat(rs.next()).isTrue();
+                assertThat(rs.getInt("film_id")).isEqualTo(filmCreated.getId());
+                assertThat(rs.getString("title")).isEqualTo("My new film");
+                assertThat(rs.getDate("release_date").toLocalDate()).isEqualTo(LocalDate.of(2019, Month.OCTOBER, 20));
+                assertThat(rs.getInt("genre_id")).isEqualTo(1);
+                assertThat(rs.getInt("duration")).isEqualTo(123);
+                assertThat(rs.getString("director")).isEqualTo("my new director");
+                assertThat(rs.getString("summary")).isEqualTo("my new summary");
+                assertThat(rs.next()).isFalse();
+            }
+        }
+    }
+*/
 }
